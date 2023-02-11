0ee80d40026615d8de16a5aa6790e59f58058661
Assolita <118940874+Assolita@users.noreply.github.com>
2023-02-11 00:03:54 -0300

    mensagem de boas vindas

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     welcome: (req, res) => {
-        res.json("bem-vindo à API da clínica de saúde mental la-vie! conteúdo sigiloso, shh..")
+        res.json("bem-vindo à API da clínica de saúde mental la-vie! Faça o seu registro, efetue o login e comece a navegar pela API. Não esqueça de utilizar o token gerado, para provar que é um usuário autenticado!")
     
     }
 }
